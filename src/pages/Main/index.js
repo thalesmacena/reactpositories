07db0d995a6eaa5e36ca0e36c4e0a52ef824a656
Thales Macena <thalesmacena@gmail.com>
2020-12-07 23:24:30 -0300

    Add Main page

---
+++ src/pages/Main/index.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Container from '../../components/Container';
+import api from '../../services/api';
+import { Form, List, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newRepo: '',
+      repos: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    const repos = localStorage.getItem('repos');
+
+    if (repos) {
+      this.setState({ repos: JSON.parse(repos) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repos } = this.state;
+    if (prevState.repos !== repos) {
+      localStorage.setItem('repos', JSON.stringify(repos));
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repos } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repos: [...repos, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading, repos } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repos.map((repository) => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
